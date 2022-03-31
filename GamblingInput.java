e9f598b70ec17bd899685dfa6c53fdf60289c214
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-03-31 09:12:39 +0530

    selection of win and loose

--- GamblingInput.java
+++ GamblingInput.java
@@ -6,9 +6,18 @@ public class GamblingInput {
 	
 	public static void main(String[] args) {
 	
-			System.out.println("Welcome to the Gambling Simulation Game");
-			
+		System.out.println("Welcome to the Gambling Simulation Game");
+		System.out.println("The Gaming bit start with 1 $");
+		 
+		
+			if (Math.random()<0.5) {
+				System.out.println("Player Wins");
+			}
+				else
+				System.out.println("Player loose");		
+		}
+		
 
 	}
 
-}
+
